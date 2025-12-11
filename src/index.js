5a354098b75bb55e78c34e56ff29eeb74a9c129f
donato.desia <donato@abk.com>
2025-12-11 11:08:38 +0100

    Add Pipes Group / Add Pipes Movement

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import Phaser from 'phaser';
 
 const config = {  // WebGL (Web graphics library): Js Api for rendering 2D and 3D grapichs.
   type: Phaser.AUTO,
-  width: 800,
+  width: 1800,
   height: 600,
   physics: {
     default: 'arcade',
@@ -25,57 +25,88 @@ function preload () {
 
 const velo = 200;
 let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
 let toolDelta = null;
 let flapVelo = 250;
 const initialBirdPosition = {x: config.width / 7.5, y: config.height / 2};
-let pipePosition = {x: null, y: null}
-const pipeGapRange = [150, 250];
-let pipeGap = Phaser.Math.Between(...pipeGapRange);
 
+let pipesGroup = null;
+let pipePairs = [];
+
+// Render Sprites:
 function create () {
+
+  //Sky
   this.add.image(400, 300, "sky");
+
+  //Bird
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0); 
   bird.body.gravity.y = 400;
-  upperPipe = this.physics.add.sprite(pipePosition.x = config.width -500, pipePosition.y = config.height - 850, 'pipe').setOrigin(0);
-  lowerPipe = this.physics.add.sprite(pipePosition.x = config.width -500, pipePosition.y = config.height - 250, 'pipe').setOrigin(0);
 
-  
+  //Flap
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown-SPACE', flap);
-}
 
-function createPipe() {
-}
-
-// if bird position x is same or larger than width of canvas, go back to left.
-// if else bird position x is smaller or equal to 0, then move back to the right.
-  // if(bird.x >= config.width) { 
-  //   bird.body.velocity.x = -velo; }
-  // else if(bird.x <= 0) {
-  //   bird.body.velocity.x = velo; }
-
-/* If Bird y position is smaller than 0 or greater than height of the canvas
-   Then ALERt: "You lost" */
-
-function update(time, delta){  // "- bird.height" doesn´t work
-  if(bird.y >= config.height || bird.y <= 0 - bird.height){
-  //  alert("You lost! Try Again");
-    restart();
+  //Pipes
+  pipesGroup = this.physics.add.group();
+  let distanceX = 0;
+  for(let i = 1; i <= 4; i++){   
+    // Pipes Horizontal Distance
+    distanceX = i * Phaser.Math.Between(300, 400);
+    console.log(distanceX);
+    // Pipes Vertical Positions
+    let upperPipeY = Phaser.Math.Between(100, 400);
+    let lowerPipeY = Phaser.Math.Between(70, 200);
+    // Spawn Pipes
+    let upperPipe = pipesGroup.create(distanceX, upperPipeY, 'pipe').setOrigin(0, 1);
+    let lowerPipe = pipesGroup.create(distanceX, upperPipeY + lowerPipeY, 'pipe').setOrigin(0, 0);
+    // Save Pair
+    let pair = {
+      uPipe: upperPipe,
+      lPipe: lowerPipe
+    };
+    pipePairs.push(pair);
+    console.log(pipePairs[0].uPipe);
   }
+  // Move all Pipes at once
+  pipesGroup.setVelocityX(-velo);
 
+  //Don´t know yet
+    // // Colisión bird <-> pipes (opcional, ajusta)
+    // this.physics.add.collider(this.bird, this.pipesGroup, this.hitPipe, null, this);
+
+    // // Opcional: límites mundo para bird
+    // this.bird.body.collideWorldBounds = true;
 }
- 
+
+function update(time, delta){ 
+  // Update Pipes
+  pipePairs.forEach(pair => {
+    if(pair.uPipe.x < 0){ 
+      let farX = Math.max(...pipePairs.map(p => p.uPipe.x)); // Find farthest Pipes
+      let newX = farX + Phaser.Math.Between(300, 400);       // Assign Pipes new position
+      let newY = Phaser.Math.Between(100, 400);
+      let gap = Phaser.Math.Between(70, 200);
+      pair.uPipe.x = newX;
+      pair.uPipe.y = newY;
+      pair.lPipe.x = newX;
+      pair.lPipe.y = newY + gap;
+      // console.log(pair.lPipe.y);
+    }
+  })
+
+  // Update Bird
+  if(bird.y >= config.height - bird.height || bird.y <= - bird.height + 20){
+  // alert("You lost! Try Again");
+  restart();
+  }
+}
+
 function restart(){
   bird.x = initialBirdPosition.x;
   bird.y = initialBirdPosition.y;
   bird.body.velocity.y = 0;
 }
 
-function again(){
- return;
-}
 
 function flap() {
   debugger
@@ -83,41 +114,3 @@ function flap() {
 }
 
 new Phaser.Game(config);
-
-
-
-
-
-
-
-
-/*
-import Phaser from "phaser";
-
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
-  },
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
-
-new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-  this.add.sprite(config.width /2, config.height / 2, 'bird').setOrigin(0);
-}
-*/
